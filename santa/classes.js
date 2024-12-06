06647e3e2903570cf501afa4524727cc0fba0f26
Olah Aron Daniel <olah.aron@BOLYAI.LOCAL>
2024-12-06 12:14:41 +0100

    Task 1-5

--- santa/classes.js
+++ santa/classes.js
@@ -1,7 +1,28 @@
 class Factory{
     // TODO 1, 2, 3, 4, 9, 10
-   }
+    constructor() { 
+        this.manokList = []
+
+    } //
+    addMano(mano){
+        this.manokList.push(mano)
+    }
+}
    
    class Companion{
     // TODO 5
+    constructor(id,veznev,kernev,reszleg){
+      this.id = id
+      this.veznev = veznev
+      this.kernev = kernev
+      this.reszleg = reszleg  
+      this.productList = [] //mivel nemszükséges a példányétre hozásakor
+    }
+    getName(){ // nem kell parméter class ra hivatkozik
+        return this.veznev + " " +this.kernev
+    }
+    doHozzafuz(product) {
+        this.productList.push(product)
+    }
+    
    }
