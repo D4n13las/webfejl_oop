e719dcf04dff71151da6e22765e87516a45c970a
Olah Aron Daniel <olah.aron@BOLYAI.LOCAL>
2024-12-13 12:37:54 +0100

    Stuff more  added

--- table/scripts.js
+++ table/scripts.js
@@ -19,3 +19,37 @@ const array = [
         lastname: 'Horváth'
     },
 ]
+
+class Person{
+    constructor(obj){
+        this.firstname1 = obj.firstname1
+        this.firstname2 = obj.firstname2
+        this.lastname = obj.lastname
+    }
+    render(parentElement){
+        const sor = document.createElement("tr")
+        parentElement.appendChild(sor)
+        const cell = document.createElement("td")
+        sor.appendChild(cell)
+        if(this.firstname2 == undefined){
+            cell.colSpan = 2
+        }
+        else{
+            const cell2 = document.createElement("td")
+            cell2.innerText = this.firstname2
+            sor.appendChild(cell2)
+        }
+        cell.innerText = this.firstname1
+        const lastname = document.createElement("td")
+        lastname.innerText = this.lastname
+        sor.appendChild(lastname)
+    }
+}
+innit()
+function innit(){
+    const tbody = document.getElementById('tbodyId')
+    for(const obj of array){
+        const  lls = new Person(obj)
+        lls.render(tbody)
+    }
+}
